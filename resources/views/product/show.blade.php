70e6518f08045f573642891cc5d06941f4d2fb78
Huỳnh Thế Nghĩa <0306241130@caothang.edu.vn>
2026-08-12 20:46:51 +0700

    form tai file

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -5,7 +5,7 @@
 <div class="card mb-3">
     <div class="row g-0">
         <div class="col-md-4">
-            <img src="{{ asset('/img/'.$viewData["product"]->getImage()) }}"
+            <img src="{{ asset('/img/'.$viewData['product']->getImage()) }}"
 
                 class="img-fluid rounded-start">
         </div>
